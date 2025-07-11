9787ee09ddf4de8a4819518af9fdac94dd453ef1
Mohamed EL AYADI <moh.el.ayadi@gmail.com>
2025-07-11 11:04:40 +0100

    checkpoint 2

--- src/components/FolderItem.tsx
+++ src/components/FolderItem.tsx
@@ -1,15 +1,22 @@
-import { useState } from 'react';
-import { Folder } from '../types';
+import {useState} from 'react';
+import {Folder} from '../types';
 import SectionItem from './SectionItem';
 
 interface FolderItemProps {
   folder: Folder;
   activeSection: string | null;
   onAddSection: (folderId: string, name: string) => void;
-  onAddSaveItem: (folderId: string, sectionId: string, name: string, value: string, sensitive?: boolean) => void;
+  onAddSaveItem: (
+    folderId: string, sectionId: string, name: string, value: string,
+    sensitive?: boolean
+  ) => void;
   onUpdateSection: (folderId: string, sectionId: string, name: string) => void;
-  onUpdateSaveItem: (folderId: string, sectionId: string, itemId: string, name: string, value: string, sensitive?: boolean) => void;
-  onDeleteSaveItem: (folderId: string, sectionId: string, itemId: string) => void;
+  onUpdateSaveItem: (
+    folderId: string, sectionId: string, itemId: string, name: string,
+    value: string, sensitive?: boolean
+  ) => void;
+  onDeleteSaveItem: (
+    folderId: string, sectionId: string, itemId: string) => void;
   onDeleteSection: (folderId: string, sectionId: string) => void;
   onSelectSection: (sectionId: string | null) => void;
 }
@@ -44,8 +51,11 @@ const FolderItem: React.FC<FolderItemProps> = ({
           className="flex items-center text-blue-500 hover:text-blue-600 bg-blue-50 hover:bg-blue-100 dark:bg-blue-900 dark:hover:bg-blue-800 px-2 py-1 rounded"
           onClick={() => setIsAddingSection(true)}
         >
-          <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5 mr-1" viewBox="0 0 20 20" fill="currentColor">
-            <path fillRule="evenodd" d="M10 3a1 1 0 00-1 1v5H4a1 1 0 100 2h5v5a1 1 0 102 0v-5h5a1 1 0 100-2h-5V4a1 1 0 00-1-1z" clipRule="evenodd" />
+          <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5 mr-1"
+               viewBox="0 0 20 20" fill="currentColor">
+            <path fillRule="evenodd"
+                  d="M10 3a1 1 0 00-1 1v5H4a1 1 0 100 2h5v5a1 1 0 102 0v-5h5a1 1 0 100-2h-5V4a1 1 0 00-1-1z"
+                  clipRule="evenodd"/>
           </svg>
           Add Section
         </button>
@@ -116,53 +126,6 @@ const FolderItem: React.FC<FolderItemProps> = ({
                   >
                     {section.name}
                   </button>
-                  <div className="absolute top-0 right-0 flex opacity-0 group-hover:opacity-100 focus-within:opacity-100">
-                    <button
-                      className="ml-1 text-blue-500 hover:text-blue-600"
-                      onClick={(e) => {
-                        e.stopPropagation();
-                        if (activeSection === section.id) {
-                          const sectionItem = document.getElementById(`section-${section.id}`);
-                          if (sectionItem) {
-                            const editButton = sectionItem.querySelector('button[title="Rename section"]');
-                            if (editButton) {
-                              (editButton as HTMLButtonElement).click();
-                            }
-                          }
-                        } else {
-                          onSelectSection(section.id);
-                          setTimeout(() => {
-                            const sectionItem = document.getElementById(`section-${section.id}`);
-                            if (sectionItem) {
-                              const editButton = sectionItem.querySelector('button[title="Rename section"]');
-                              if (editButton) {
-                                (editButton as HTMLButtonElement).click();
-                              }
-                            }
-                          }, 100);
-                        }
-                      }}
-                      title="Edit section"
-                    >
-                      <svg xmlns="http://www.w3.org/2000/svg" className="h-4 w-4" viewBox="0 0 20 20" fill="currentColor">
-                        <path d="M13.586 3.586a2 2 0 112.828 2.828l-.793.793-2.828-2.828.793-.793zM11.379 5.793L3 14.172V17h2.828l8.38-8.379-2.83-2.828z" />
-                      </svg>
-                    </button>
-                    <button
-                      className="ml-1 text-red-500 hover:text-red-600"
-                      onClick={(e) => {
-                        e.stopPropagation();
-                        if (confirm(`Are you sure you want to delete the section "${section.name}"?`)) {
-                          onDeleteSection(folder.id, section.id);
-                        }
-                      }}
-                      title="Delete section"
-                    >
-                      <svg xmlns="http://www.w3.org/2000/svg" className="h-4 w-4" viewBox="0 0 20 20" fill="currentColor">
-                        <path fillRule="evenodd" d="M9 2a1 1 0 00-.894.553L7.382 4H4a1 1 0 000 2v10a2 2 0 002 2h8a2 2 0 002-2V6a1 1 0 100-2h-3.382l-.724-1.447A1 1 0 0011 2H9zM7 8a1 1 0 012 0v6a1 1 0 11-2 0V8zm5-1a1 1 0 00-1 1v6a1 1 0 102 0V8a1 1 0 00-1-1z" clipRule="evenodd" />
-                      </svg>
-                    </button>
-                  </div>
                 </div>
               ))}
             </nav>
--- src/components/SectionItem.tsx
+++ src/components/SectionItem.tsx
@@ -19,6 +19,7 @@ const SectionItem: React.FC<SectionItemProps> = ({
   onUpdateSection,
   onUpdateSaveItem,
   onDeleteSaveItem,
+  onDeleteSection,
 }) => {
   const [isAddingItem, setIsAddingItem] = useState(false);
   const [newItemName, setNewItemName] = useState('');
@@ -80,16 +81,46 @@ const SectionItem: React.FC<SectionItemProps> = ({
             </button>
           </form>
         ) : (
-          <button
-            className="flex items-center text-blue-500 hover:text-blue-600 bg-blue-50 hover:bg-blue-100 dark:bg-blue-900 dark:hover:bg-blue-800 px-2 py-1 rounded"
-            onClick={() => setIsAddingItem(true)}
-            title="Add new item"
-          >
-            <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5 mr-1" viewBox="0 0 20 20" fill="currentColor">
-              <path fillRule="evenodd" d="M10 3a1 1 0 00-1 1v5H4a1 1 0 100 2h5v5a1 1 0 102 0v-5h5a1 1 0 100-2h-5V4a1 1 0 00-1-1z" clipRule="evenodd" />
-            </svg>
-            Add Item
-          </button>
+          <>
+            <div className="flex space-x-2">
+              <button
+                className="flex items-center text-blue-500 hover:text-blue-600 bg-blue-50 hover:bg-blue-100 dark:bg-blue-900 dark:hover:bg-blue-800 px-2 py-1 rounded"
+                onClick={() => setIsAddingItem(true)}
+                title="Add new item"
+              >
+                <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5 mr-1" viewBox="0 0 20 20" fill="currentColor">
+                  <path fillRule="evenodd" d="M10 3a1 1 0 00-1 1v5H4a1 1 0 100 2h5v5a1 1 0 102 0v-5h5a1 1 0 100-2h-5V4a1 1 0 00-1-1z" clipRule="evenodd" />
+                </svg>
+                Add Item
+              </button>
+            </div>
+            <div className="flex gap-2 items-center">
+              <button
+                className="flex items-center text-gray-500 hover:text-gray-200 bg-gray-50 hover:bg-gray-100 dark:bg-gray-700 dark:hover:bg-gray-600 px-2 py-1 rounded"
+                onClick={() => setIsEditingSection(true)}
+                title="Edit section"
+              >
+                <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5 mr-1" viewBox="0 0 20 20" fill="currentColor">
+                  <path d="M13.586 3.586a2 2 0 112.828 2.828l-.793.793-2.828-2.828.793-.793zM11.379 5.793L3 14.172V17h2.828l8.38-8.379-2.83-2.828z" />
+                </svg>
+                Rename section
+              </button>
+              <button
+                className="flex items-center text-red-500 hover:text-red-600 bg-red-50 hover:bg-red-100 dark:bg-red-900 dark:hover:bg-red-800 px-2 py-1 rounded"
+                onClick={() => {
+                  if (window.confirm(`Are you sure you want to delete the section "${section.name}"?`)) {
+                    onDeleteSection(folderId, section.id);
+                  }
+                }}
+                title="Delete section"
+              >
+                <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5 mr-1" viewBox="0 0 20 20" fill="currentColor">
+                  <path fillRule="evenodd" d="M9 2a1 1 0 00-.894.553L7.382 4H4a1 1 0 000 2v10a2 2 0 002 2h8a2 2 0 002-2V6a1 1 0 100-2h-3.382l-.724-1.447A1 1 0 0011 2H9zM7 8a1 1 0 012 0v6a1 1 0 11-2 0V8zm5-1a1 1 0 00-1 1v6a1 1 0 102 0V8a1 1 0 00-1-1z" clipRule="evenodd" />
+                </svg>
+                Delete
+              </button>
+            </div>
+          </>
         )}
       </div>
 
